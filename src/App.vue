c823666cfa1abe30c234e0e77854a57d288bdd01
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-04-17 12:48:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+    import axios from 'axios';
+    import { store } from './store.js';
+
+export default{
+    data (){
+    return{
+        store
+    }
+    },
+    methods: {
+
+    },
+    mounted (){
+
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>ciao sono appvue</h1> 
+    </div>
+</template>
+
+<style lang="scss">
+    @use './style/generic';
+    @use './style/partials/variables' as *;
+
+h1{
+    color: $primary-color;
+}
+
+</style>
